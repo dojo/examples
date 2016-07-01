42c0c7270a7bb3a7baaadbb9061dd906467a448e
Anthony Gubler <agubler@sitepen.com>
2016-07-01 18:29:15 +0100

    add delete and toggle todos

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,104 @@
-export {
-	/* provide the public API here */
-};
+import createApp from 'dojo-app/createApp';
+import { AnyAction } from 'dojo-actions/createAction';
+import createPanel from 'dojo-widgets/createPanel';
+import createTextInput from 'dojo-widgets/createTextInput';
+import createWidget from 'dojo-widgets/createWidget';
+import createMemoryStore from './utils/createLocalMemoryStore';
+
+import * as storeTodoActions from './actions/storeTodoActions';
+import * as uiTodoActions from './actions/uiTodoActions';
+import todoRegistryFactory from './registry/createTodoRegistry';
+import createTodoList from './widgets/createTodoList';
+
+const todoStore = createMemoryStore({
+	data: [
+		{id: 'todo1', label: 'My First Todo!', completed: false}
+	]
+});
+
+const widgetStore = createMemoryStore({
+	data: [
+		{id: 'title', label: 'todos'},
+		{id: 'new-todo', classes: ['new-todo'], placeholder: 'What needs to be done?'},
+		{id: 'main-section', classes: ['main']},
+		{id: 'todo-list', classes: ['todo-list'], children: []}
+	]
+});
+
+todoStore.observe().subscribe((options: any) => {
+	const { deleted, item } = options;
+	if (deleted) {
+		widgetStore.get('todo-list').then((todoList: any) => {
+			const children = todoList.children.filter((id: string) => id !== item);
+			return widgetStore.delete(item).patch({id: todoList.id, children});
+		});
+	} else {
+		widgetStore.get('todo-list').then((todoList: any) => {
+			const { id, children } = todoList;
+
+			function put() {
+				return widgetStore
+					.put(item)
+					.patch({id, children: [...children, item.id]});
+			}
+
+			function patch() {
+				return widgetStore
+					.patch(item)
+					.patch({id, children});
+			}
+
+			return children.includes(item.id) ? patch() : put();
+		});
+	}
+});
+
+const app = createApp({ defaultStore: widgetStore });
+
+app.registerStore('widget-store', widgetStore);
+app.registerStore('todo-store', todoStore);
+
+Object.keys(storeTodoActions).forEach((actionName) => {
+	const action: AnyAction = (<any> storeTodoActions)[actionName];
+	app.registerAction(actionName, action);
+});
+
+Object.keys(uiTodoActions).forEach((actionName) => {
+	const action: AnyAction = (<any> uiTodoActions)[actionName];
+	app.registerAction(actionName, action);
+});
+
+app.loadDefinition({
+	widgets: [
+		{
+			id: 'new-todo',
+			factory: createTextInput,
+			listeners: {
+				keypress: 'todoInput'
+			}
+		},
+		{
+			id: 'main-section',
+			factory: createPanel,
+			options: {
+				tagName: 'section'
+			}
+		},
+		{
+			id: 'todo-list',
+			factory: createTodoList,
+			options: {
+				widgetRegistry: todoRegistryFactory({ widgetStore })
+			}
+		}
+	],
+	customElements: [
+		{
+			name: 'dojo-widget',
+			factory: createWidget
+		}
+	]
+});
+
+app.realize(document.body);
+
