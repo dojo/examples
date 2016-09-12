6157586b0196c971c2a0e34697c42e55fa299ab5
Tom Dye <tom@ursaminor.co.uk>
2016-09-12 12:19:23 +0100

    remove node options from grunt

--- dojo-cli-example/Gruntfile.js
+++ dojo-cli-example/Gruntfile.js
@@ -1,11 +1,6 @@
 module.exports = function (grunt) {
 	require('grunt-dojo2').initConfig(grunt, {
 		/* any custom configuration goes here */
-		intern: {
-			options: {
-				nodeOptions: [  ]
-			}
-		}
 	});
 
 	grunt.registerTask('ci', [
