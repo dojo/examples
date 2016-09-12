bc042beb0daf793027e0e72b3830685bf6121bfa
Tom Dye <tom@ursaminor.co.uk>
2016-09-12 12:17:09 +0100

    100% test coverage

--- dojo-cli-example/Gruntfile.js
+++ dojo-cli-example/Gruntfile.js
@@ -1,6 +1,11 @@
 module.exports = function (grunt) {
 	require('grunt-dojo2').initConfig(grunt, {
 		/* any custom configuration goes here */
+		intern: {
+			options: {
+				nodeOptions: [  ]
+			}
+		}
 	});
 
 	grunt.registerTask('ci', [
