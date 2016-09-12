0085a6e9443f9f2a32b364df40898b217ea758e1
Tom Dye <Tomdye@users.noreply.github.com>
2016-09-12 16:45:53 +0100

    Adds example `cli command` (#36)
    * created example module
    
    * remove cli dependency
    
    * fix tab
    
    * 100% test coverage
    
    * 100% test coverage
    
    * remove node options from grunt
    
    * remove node options from grunt
    
    * rewrote output

--- dojo-cli-example/src/main.ts
+++ dojo-cli-example/src/main.ts
@@ -1,3 +1,38 @@
-export {
-	/* provide the public API here */
+import { Command, Helper } from './interfaces';
+import { Argv } from 'yargs';
+
+export interface MyContext {
+	message?: string;
+}
+
+export interface MyArgv extends Argv {
+	shout: boolean;
+}
+
+const command: Command = {
+	description: 'This is an example dojo-cli command',
+	register(helper) {
+		// Put something in the context
+		helper.context.message = 'Hello World!';
+
+		// Return the yargs instance
+		helper.yargs.option('s', {
+			alias: 'shout',
+			describe: 'SHOUT the response'
+		});
+
+		return helper.yargs;
+	},
+	run(helper: Helper, args: MyArgv) {
+		const message = (<MyContext> helper.context).message;
+
+		if (message) {
+			const outputMessage = args.shout ? message.toUpperCase() : message;
+			console.log(outputMessage);
+		}
+
+		return new Promise((resolve) => setTimeout(resolve, 500));
+	}
 };
+
+export default command;
