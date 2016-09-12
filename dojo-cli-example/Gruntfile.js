c17df04393d50751a25afeae643e44735f748b61
Tom Dye <Tomdye@users.noreply.github.com>
2016-09-12 13:45:49 +0100

    travis builds (#38)

--- dojo-cli-example/Gruntfile.js
+++ dojo-cli-example/Gruntfile.js
@@ -2,4 +2,8 @@ module.exports = function (grunt) {
 	require('grunt-dojo2').initConfig(grunt, {
 		/* any custom configuration goes here */
 	});
+
+	grunt.registerTask('ci', [
+		'intern:node'
+	]);
 };
