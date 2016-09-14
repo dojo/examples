9d859bc8f071f68c3b901ae66ccb91af0241ef6f
Anthony Gubler <agubler@sitepen.com>
2016-09-14 18:30:50 +0100

    footer

--- monster-cards/src/main.ts
+++ monster-cards/src/main.ts
@@ -1,13 +1,18 @@
 import createApp from 'dojo-app/createApp';
 import createMemoryStore from 'dojo-stores/createMemoryStore';
 
-import createNavbar from './widgets//navbar/createNavbar';
+import createNavbar from './widgets/navbar/createNavbar';
+import createFooter from './widgets/footer/createFooter';
 
 const widgetStore = createMemoryStore({
 	data: [
 		{
 			id: 'navbar',
 			classes: [ 'navbar' ]
+		},
+		{
+			id: 'footer',
+			classes: [ 'footer' ]
 		}
 	]
 });
@@ -19,6 +24,10 @@ app.loadDefinition({
 		{
 			id: 'navbar',
 			factory: createNavbar
+		},
+		{
+			id: 'footer',
+			factory: createFooter
 		}
 	]
 });
---
+++ monster-cards/src/widgets/common/createImage.ts
@@ -0,0 +1,35 @@
+import { ComposeFactory } from 'dojo-compose/compose';
+import createWidget, { Widget, WidgetState, WidgetOptions } from 'dojo-widgets/createWidget';
+import { VNodeProperties } from 'maquette';
+import createCachedRenderMixin from 'dojo-widgets/mixins/createCachedRenderMixin';
+
+export interface ImageState extends WidgetState {
+	src?: string;
+}
+
+export interface ImageOptions extends WidgetOptions<ImageState> { }
+
+export type Image = Widget<ImageState>;
+
+export interface ImageFactory extends ComposeFactory<Image, ImageOptions> { }
+
+const createImage: ImageFactory = createWidget
+	.mixin({
+		mixin: createCachedRenderMixin,
+		aspectAdvice: {
+			before: {
+				getNodeAttributes(this: Image, overrides: VNodeProperties = {}): VNodeProperties[] {
+					if (this.state.src !== undefined) {
+						overrides.src = this.state.src;
+					}
+
+					return [overrides];
+				}
+			}
+		}
+	})
+	.extend({
+		tagName: 'img'
+	});
+
+export default createImage;
