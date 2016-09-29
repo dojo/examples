f05444360556febeb28d479a25a6ff40af8ceb47
Mark Wubben <mark@novemberborn.net>
2016-09-29 14:30:16 +0100

    todo-mvc: Refactor user actions
    Better typing of do() arguments. Return promises. Whitespace.

--- todo-mvc/src/actions/userActions.ts
+++ todo-mvc/src/actions/userActions.ts
@@ -1,6 +1,7 @@
 import createAction from 'dojo-actions/createAction';
 import { assign } from 'dojo-core/lang';
 
+import { Item } from '../stores/todoStore';
 import widgetStore from '../stores/widgetStore';
 import {
 	addTodo,
@@ -10,74 +11,96 @@ import {
 	updateTodo
 } from './todoStoreActions';
 
+interface FormEvent extends Event {
+	target: HTMLInputElement;
+}
+
+interface FormInputEvent extends KeyboardEvent {
+	target: HTMLInputElement;
+}
+
 export const todoInput = createAction({
-	do(options: any) {
-		if (options.event.keyCode === 13 && options.event.target.value) {
-			addTodo.do({label: options.event.target.value, completed: false});
-			widgetStore.patch({id: 'new-todo', value: ''});
+	do({
+		event: {
+			keyCode,
+			target: { value: label }
+		}
+	}: { event: FormInputEvent }) {
+		if (keyCode === 13 && label) {
+			addTodo.do({ label, completed: false });
+			return widgetStore.patch({ id: 'new-todo', value: '' });
 		}
 	}
 });
 
 export const todoEdit = createAction({
 	do(options: any) {
-		widgetStore.patch(assign(options, {editing: true}));
+		return widgetStore.patch(assign(options, { editing: true }));
 	}
 });
 
 export const todoEditInput = createAction({
-	do(options: any) {
-		if (options.event.keyCode === 13) {
-			todoSave.do(options);
+	do(options: { event: FormInputEvent, state: any }) {
+		const { event: { keyCode } } = options;
+		if (keyCode === 13) {
+			return todoSave.do(options);
 		}
-		else if (options.event.keyCode === 27) {
-			widgetStore.patch(assign(options.state, {editing: false}));
+		else if (keyCode === 27) {
+			return widgetStore.patch(assign(options.state, { editing: false }));
 		}
 	}
 });
 
 export const todoSave = createAction({
-	do(options: any) {
-		const label = options.event.target.value;
-
+	do({
+		event: {
+			target: { value: label }
+		},
+		state
+	}: { event: FormInputEvent, state: any }) {
 		if (!label) {
-			deleteTodo.do({id: options.state.id});
+			return deleteTodo.do(state);
 		}
 		else {
-			updateTodo.do(assign(options.state, {label, editing: false}));
+			return updateTodo.do(assign(state, { label, editing: false }));
 		}
 	}
 });
 
 export const todoRemove = createAction({
-	do(options: {id: string}) {
-		deleteTodo.do(options);
+	do(item: Item) {
+		return deleteTodo.do(item);
 	}
 });
 
 export const todoToggleComplete = createAction({
-	do(options: any) {
-		const item = assign({}, options, { completed: !options.completed });
-		updateTodo.do(item);
+	do(item: Item) {
+		item = assign({}, <any> item, { completed: !item.completed });
+		return updateTodo.do(item);
 	}
 });
 
 export const filter = createAction({
-	do(options: {filter: string}) {
-		widgetStore.patch({id: 'todo-footer', activeFilter: options.filter});
-		widgetStore.patch({id: 'todo-list', activeFilter: options.filter});
+	do({ filter: activeFilter }: {filter: string}) {
+		return Promise.all([
+			widgetStore.patch({ id: 'todo-footer', activeFilter }),
+			widgetStore.patch({ id: 'todo-list', activeFilter })
+		]);
 	}
 });
 
 export const todoToggleAll = createAction({
-	do(options: any) {
-		const checked = options.event.target.checked;
-		toggleAll.do({checked});
+	do({
+		event: {
+			target: { checked }
+		}
+	}: { event: FormEvent }) {
+		toggleAll.do({ checked });
 	}
 });
 
 export const clearCompleted = createAction({
 	do() {
-		deleteCompleted.do();
+		return deleteCompleted.do();
 	}
 });
