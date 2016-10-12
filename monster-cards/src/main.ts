90321d167fed38d5d63ca555787c121274052fe4
Tom Dye <tom@ursaminor.co.uk>
2016-10-12 10:05:46 +0100

    trying to add card-details

--- monster-cards/src/main.ts
+++ monster-cards/src/main.ts
@@ -3,6 +3,7 @@ import createMemoryStore from 'dojo-stores/createMemoryStore';
 import createWidget from 'dojo-widgets/createWidget';
 
 import createNavbar from './widgets/navbar/createNavbar';
+import createCardDetails from './widgets/cardDetails/createCardDetails';
 
 let text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis dolor euismod, consequat nisl in, condimentum odio. Vivamus sit amet ipsum volutpat, auctor risus sit amet, consequat libero. Etiam quis suscipit ipsum, sit amet porta lectus. In sollicitudin orci quis tempus bibendum. Nam non ex in est cursus elementum. Praesent imperdiet ante eget leo varius porta. Quisque mollis ipsum eu augue efficitur dignissim. Sed a fermentum metus. In pretium nisi odio, eu aliquam purus interdum eget. Nulla ut mi at felis facilisis porttitor facilisis quis massa. Integer egestas neque sem, non ultrices ex blandit sit amet. Sed eu lectus urna.';
 
@@ -16,26 +17,52 @@ const widgetStore = createMemoryStore({
 			id: 'navbar',
 			classes: [ 'navbar' ]
 		},
+		// {
+		// 	id: 'container',
+		// 	classes: [ 'content' ],
+		// 	label: text
+		// },
 		{
-			id: 'container',
-			classes: [ 'content' ],
-			label: text
+			id: 'cardDetails'
 		}
 	]
 });
 
+const cards: { name: string }[] = [];
+for (let i = 0; i < 10; i++) {
+	cards.push({
+		name: `card-${i}`
+	});
+}
+
+const cardStore = createMemoryStore({
+	data: cards
+});
+
 const app = createApp({ defaultWidgetStore: widgetStore });
 
+app.registerStore('card-store', cardStore);
+
 app.loadDefinition({
+	customElements: [
+		{
+			name: 'card-details',
+			factory: createCardDetails
+		}
+	],
 	widgets: [
 		{
 			id: 'navbar',
 			factory: createNavbar
 		},
-		{
-			id: 'container',
-			factory: createWidget
-		}
+		// {
+		// 	id: 'container',
+		// 	factory: createWidget
+		// }
+		// {
+		// 	id: 'cardDetails',
+		// 	factory: createCardDetails
+		// }
 	]
 });
 
---
+++ monster-cards/src/widgets/cardDetails/createNavCard.ts
@@ -0,0 +1,8 @@
+import createRenderMixin from 'dojo-widgets/mixins/createRenderMixin';
+
+const create = createRenderMixin
+	.extend({
+		tagName: 'li'
+	});
+
+export default create;
