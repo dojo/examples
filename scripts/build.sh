e097843d0c4bfa74a0a5f4406dea23ff56811e95
Anthony Gubler <agubler@sitepen.com>
2016-10-31 12:46:12 +0000

    target grunt

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,8 @@ if [ "$PROJECT_DIR" = "auto-deploy" ]
 then
 	echo "Skipping build for deployment matrix."
 else
-	grunt
-	grunt ci --combined
-	grunt remapIstanbul:ci
+	./node_modules/.bin/grunt
+	./node_modules/.bin/grunt ci --combined
+	./node_modules/.bin/grunt remapIstanbul:ci
 	cd ..
 fi
