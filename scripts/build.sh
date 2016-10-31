3ffe7c9e141aef9a5f91e1752613c16d9feab0b3
Anthony Gubler <agubler@sitepen.com>
2016-10-31 12:54:29 +0000

    cd into directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@ if [ "$PROJECT_DIR" = "auto-deploy" ]
 then
 	echo "Skipping build for deployment matrix."
 else
+	cd $PROJECT_DIR
 	./node_modules/.bin/grunt
 	./node_modules/.bin/grunt ci --combined
 	./node_modules/.bin/grunt remapIstanbul:ci
