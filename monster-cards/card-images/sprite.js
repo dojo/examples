23e7f9b78fec1cd2da89cab74153447a1a7b894a
Tom Dye <tom@ursaminor.co.uk>
2016-11-02 15:22:25 +0000

    reworked sprite script

---
+++ monster-cards/card-images/sprite.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+'use strict';
+
+const sprity = require('sprity');
+
+const largeFileName = 'card-sprite-large';
+const smallFileName = 'card-sprite-small';
+const spriteOutputDir = '../src/widgets/card/';
+const { assign } = Object;
+
+const defaultOptions = {
+	out: spriteOutputDir,
+	format: 'jpg',
+	margin: 0,
+	template: './stylus-template.hbs'
+};
+
+const largeOptions = {
+	src: 'large/*.png',
+	name: largeFileName,
+	style: `${largeFileName}.styl`,
+	prefix: largeFileName
+};
+
+const smallOptions = {
+	src: 'small/*.png',
+	name: smallFileName,
+	style: `${smallFileName}.styl`,
+	prefix: smallFileName
+};
+
+sprity.create(assign(defaultOptions, largeOptions), () => {
+	sprity.create(assign(defaultOptions, smallOptions));
+});
