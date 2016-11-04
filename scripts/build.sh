a85e0fb75f63d6969608a9d88714c9dfa8bb62b7
Anthony Gubler <agubler@gmail.com>
2016-11-04 10:35:19 +0000

    Fix Auto Deployment Scripts (#119)
    * fix scripts
    
    * check and bubble non zero exit codes
    
    * add fast fail

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+
+function runCommandAndCheckStatus {
+	#run command
+	"$@"
+	local status=$?
+	if [ $status -ne 0 ]; then
+		echo "error with $@" >&2
+		exit $status
+	fi
+	return $status
+}
+
 if [ "$PROJECT_DIR" = "auto-deploy" ]
 then
 	echo "Skipping build for deployment matrix."
 else
 	cd $PROJECT_DIR
-	./node_modules/.bin/grunt
-	./node_modules/.bin/grunt ci --combined
-	./node_modules/.bin/grunt remapIstanbul:ci
+	runCommandAndCheckStatus ./node_modules/.bin/grunt
+	runCommandAndCheckStatus ./node_modules/.bin/grunt ci --combined
+	runCommandAndCheckStatus ./node_modules/.bin/grunt remapIstanbul:ci
+	runCommandAndCheckStatus ./node_modules/.bin/dojo build webpack
 	cd ..
 fi
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+
+function runCommandAndCheckStatus {
+	#run command
+	"$@"
+	local status=$?
+	if [ $status -ne 0 ]; then
+		echo "error with $@" >&2
+		exit $status
+	fi
+	return $status
+}
+
 if [ "$PROJECT_DIR" = "auto-deploy" ]
 then
 	echo "Skipping install for deployment matrix."
 else
 	cd $PROJECT_DIR
-	npm install dojo-cli -g
-	npm install grunt-cli
-	npm install
+	runCommandAndCheckStatus npm install dojo-cli
+	runCommandAndCheckStatus npm install grunt-cli
+	runCommandAndCheckStatus npm install
 fi
