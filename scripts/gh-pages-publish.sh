8d6fe8abd9ecbfdf8e992e5d948050604b58e796
Anthony Gubler <agubler@sitepen.com>
2016-11-04 13:31:04 +0000

    script to not depend on previous steps

--- scripts/gh-pages-publish.sh
+++ scripts/gh-pages-publish.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
+
+function buildProject {
+	export PROJECT_DIR=$@
+	./scripts/install.sh
+	cd $PROJECT_DIR
+	npm install typings
+	./node_modules/.bin/typings install
+	./node_modules/.bin/dojo build webpack
+	cd ..
+}
+
 if [ "$PROJECT_DIR" = "auto-deploy" ]
 then
-	git checkout -B gh-pages
+	if [ ! -z "$GH_TOKEN" ]
+	then
 
-	mkdir samples
-	mkdir samples/todo-mvc
-	mkdir samples/monster-cards
+		buildProject "todo-mvc"
+		buildProject "monster-cards"
 
-	cp index.html samples/index.html
-	cp -r monster-cards/dist/* samples/monster-cards/
-	cp -r todo-mvc/dist/* samples/todo-mvc/
+		git checkout -B gh-pages
 
-	git remote add pages "https://$GH_TOKEN@github.com/dojo/examples.git"
+		mkdir samples
+		mkdir samples/todo-mvc
+		mkdir samples/monster-cards
 
-	git add -f samples
-	git commit -am "built example"
+		cp index.html samples/index.html
+		cp -r monster-cards/dist/* samples/monster-cards/
+		cp -r todo-mvc/dist/* samples/todo-mvc/
 
-	git filter-branch -f --prune-empty --subdirectory-filter samples
+		git remote add pages "https://$GH_TOKEN@github.com/dojo/examples.git"
 
-	git push -f pages gh-pages
+		git add -f samples
+		git commit -am "built example"
 
-	git checkout -
+		git filter-branch -f --prune-empty --subdirectory-filter samples
+
+		git push -f pages gh-pages
+
+		git checkout -
+	else
+		echo "No GH_TOKEN detected"
+	fi
+else
+	echo "only deploy during auto deploy matrix"
 fi
