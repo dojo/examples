5d7dbf8b710af05a6fcf23b977d2da907704c2a0
Tom Dye <tom@ursaminor.co.uk>
2016-11-07 20:31:17 +0000

    added random selected seen with cards

--- monster-cards/src/actions/routeActions.ts
+++ monster-cards/src/actions/routeActions.ts
@@ -1,11 +1,29 @@
 import createAction from 'dojo-actions/createAction';
 import widgetStore from '../stores/widgetStore';
+import { pickRandomCards, Card } from '../stores/cardStore';
+import { assign } from 'dojo-core/lang';
+
+function getCardSummaryDetails(card: Card) {
+	const { id, name, score, imageClass } = card;
+	return {
+		cardId: id,
+		name,
+		score,
+		imageClass
+	};
+};
 
 export const gotoCardDetails = createAction({
 	do({ id }) {
 		return Promise.all([
 			widgetStore.patch({ id: 'cardDetailsJumbotron', children: [ `description-${id}` ] }),
-			widgetStore.patch({ id: 'container', children: [ 'cardDetails' ] })
+			widgetStore.patch({ id: 'container', children: [ 'cardDetails' ] }),
+			pickRandomCards(2, [ id ]).then(([ card1, card2 ]) => {
+				return Promise.all([
+					widgetStore.patch(assign({ id: 'cardDetailsSeenWithCard1' }, getCardSummaryDetails(card1))),
+					widgetStore.patch(assign({ id: 'cardDetailsSeenWithCard2' }, getCardSummaryDetails(card2)))
+				]);
+			})
 		]);
 	}
 });
