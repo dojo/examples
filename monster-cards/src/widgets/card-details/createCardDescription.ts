b2f92d0fd71619f8669f9c212d1d290ad828a2b3
Tom Dye <tom@ursaminor.co.uk>
2016-11-10 17:51:59 +0000

    moving to d

--- monster-cards/src/widgets/card-details/createCardDescription.ts
+++ monster-cards/src/widgets/card-details/createCardDescription.ts
@@ -1,10 +1,7 @@
-import createRenderableChildrenMixin from 'dojo-widgets/mixins/createRenderableChildrenMixin';
-import createRenderMixin, { RenderMixin, RenderMixinState, RenderMixinOptions } from 'dojo-widgets/mixins/createRenderMixin';
+import { Widget, DNode } from 'dojo-interfaces/widgetBases';
+import createWidgetBase from 'dojo-widgets/bases/createWidgetBase';
+import d from 'dojo-widgets/util/d';
 import createCssTransitionMixin from 'dojo-widgets/mixins/createCssTransitionMixin';
-import createWidget from 'dojo-widgets/createWidget';
-import createIconLink from '../common/createIconLink';
-import { h, VNode } from 'maquette';
-import Map from 'dojo-shim/Map';
 
 export type MilestoneCardDetails = {
 	name: string;
@@ -15,115 +12,38 @@ export type MilestoneCardDetails = {
 	cardId: string;
 }
 
-export type CardDescriptionState = RenderMixinState & MilestoneCardDetails;
+export type CardDescription = Widget<MilestoneCardDetails>;
 
-export type CardDescriptionOptions = RenderMixinOptions<CardDescriptionState>;
-
-export type CardDescription = RenderMixin<CardDescriptionState>;
-
-type ChildWidgetNames =
-	'cardImage' |
-	'name' |
-	'tagline' |
-	'description' |
-	'favouriteCount' |
-	'addToFavouritesLink' |
-	'twitterLink' |
-	'facebookLink';
-
-const instanceWeakMap = new WeakMap<CardDescription, Map<ChildWidgetNames, RenderMixin<any>>>();
-
-function manageChildren(this: CardDescription) {
-	const childrenMap = instanceWeakMap.get(this);
-	childrenMap.get('favouriteCount').setState({
-		label: this.state.favouriteCount
-	});
-}
-
-const createCardDescription = createRenderMixin
+const createCardDescription = createWidgetBase
 	.mixin(createCssTransitionMixin)
-	.mixin({
-		mixin: createRenderableChildrenMixin,
-		initialize(instance: CardDescription, options: CardDescriptionOptions) {
-			const childrenMap = new Map<ChildWidgetNames, RenderMixin<any>>();
-			instanceWeakMap.set(instance, childrenMap);
-			childrenMap.set('cardImage', createWidget({
-				tagName: 'div',
-				state: {
-					classes: [ 'cardImage', 'card-sprite-large', options.state.imageClass ]
-				}
-			}));
-			childrenMap.set('name', createWidget({
-				state: {
-					label: options.state.name
-				},
-				tagName: 'h1'
-			}));
-			childrenMap.set('tagline', createWidget({
-				state: {
-					label: options.state.tagline,
-					classes: [ 'tagline' ]
-				},
-				tagName: 'strong'
-			}));
-			childrenMap.set('description', createWidget({
-				state: {
-					label: options.state.description
-				},
-				tagName: 'p'
-			}));
-			childrenMap.set('favouriteCount', createWidget({
-				state: {
-					label: options.state.favouriteCount.toString(),
-					classes: [ 'favouriteCount' ]
-				},
-				tagName: 'span'
-			}));
-			childrenMap.set('addToFavouritesLink', createIconLink({
-				state: {
-					classes: [ 'button' ],
-					iconClass: [ 'fa', 'fa-heart-o'],
-					text: 'Add to favourites'
-				}
-			}));
-			childrenMap.set('twitterLink', createIconLink({
-				state: {
-					classes: [ 'button' ],
-					href: 'http://www.twitter.com',
-					iconClass: [ 'fa', 'fa-twitter' ]
-				}
-			}));
-			childrenMap.set('facebookLink', createIconLink({
-				state: {
-					classes: [ 'button' ],
-					href: 'http://www.facebook.com',
-					iconClass: [ 'fa', 'fa-facebook' ]
-				}
-			}));
-
-			instance.on('statechange', manageChildren);
-		}
-	})
 	.extend({
 		tagName: 'card-details-description',
-		getChildrenNodes(this: CardDescription): VNode[] {
-			const childrenMap = instanceWeakMap.get(this);
-			return [
-				childrenMap.get('cardImage').render(),
-				h('article', [
-					childrenMap.get('name').render(),
-					childrenMap.get('tagline').render(),
-					childrenMap.get('description').render(),
-					h('span', 'Favourited: '),
-					childrenMap.get('favouriteCount').render(),
-					h('div.buttonHolder', [
-						childrenMap.get('addToFavouritesLink').render(),
-						childrenMap.get('twitterLink').render(),
-						childrenMap.get('facebookLink').render()
+		childNodeRenderers: [
+			function(this: CardDescription): any[] {
+				return [
+					d(createWidgetBase, { state: { classes: [ 'cardImage', 'card-sprite-large', this.state.imageClass ] }}),
+					d('article', {}, [
+						d('h1', { innerHTML: this.state.name }),
+						d('strong.tagline', { innerHTML: this.state.tagline }),
+						d('p', { innerHTML: this.state.description }),
+						d('span', { innerHTML: `Favourited: ` }),
+						d('span.favouriteCount', { innerHTML: this.state.favouriteCount.toString() }),
+						d('div.buttonHolder', {}, [
+							d('a.button', {}, [
+								d('i.fa.fa-heart-o'),
+								d('span', { innerHTML: 'Add to favourites' })
+							]),
+							d('a.button', { href: 'http://www.twitter.com' }, [
+								d('i.fa.fa-twitter')
+							]),
+							d('a.button', { href: 'https://facebook.com' }, [
+								d('i.fa.fa-facebook')
+							])
+						])
 					])
-				])
-			];
-		}
+				];
+			}
+		]
 	});
 
 export default createCardDescription;
--- monster-cards/src/widgets/card/createCardSummary.ts
+++ monster-cards/src/widgets/card/createCardSummary.ts
@@ -1,4 +1,4 @@
-import { Widget } from 'dojo-interfaces/widgetBases';
+import { Widget, DNode } from 'dojo-interfaces/widgetBases';
 import createWidgetBase from 'dojo-widgets/bases/createWidgetBase';
 import d from 'dojo-widgets/util/d';
 import createCard from './createCard';
@@ -13,12 +13,12 @@ export type CardSummaryState = {
 export type CardSummary = Widget<CardSummaryState>
 
 const createCardSummary = createWidgetBase.extend({
-	classes: [ 'cardSummary' ],
+	tagName: 'div.cardSummary',
 	childNodeRenderers: [
-		function(this: CardSummary): any[] {
+		function(this: CardSummary): DNode[] {
 			const { cardId, imageClass, name, score } = this.state;
 			return [
-				d(createCard, { state: { cardId, imageClass } }),
+				d(createCard, { state: { cardId, imageClass, large: true } }),
 				d('h2', { innerHTML: name }),
 				d('p.points', { innerHTML: `milestone points: ${score}` })
 			];
