ca050c2c50dd10842dcfbff4cbe4b302cccc60dd
Rory Mulligan <rorticus@gmail.com>
2017-01-11 07:21:12 -0500

    Removing rxjs references, moving to fixed package versions for now

--- todo-mvc-kitchensink/tests/intern.ts
+++ todo-mvc-kitchensink/tests/intern.ts
@@ -66,8 +66,7 @@ export const loaderOptions = {
 		{ name: 'dojo-stores', location: 'node_modules/dojo-stores' },
 		{ name: 'dojo-widgets', location: 'node_modules/dojo-widgets' },
 		{ name: 'immutable', location: 'node_modules/immutable/dist', main: 'immutable' },
-		{ name: 'maquette', location: 'node_modules/maquette/dist', main: 'maquette' },
-		{ name: 'rxjs', location: 'node_modules/@reactivex/rxjs/dist/amd' }
+		{ name: 'maquette', location: 'node_modules/maquette/dist', main: 'maquette' }
 	]
 };
 
