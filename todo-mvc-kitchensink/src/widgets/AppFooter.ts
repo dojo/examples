93661489868728d5a7cae2df38bd1f1b63aa50ca
Rory Mulligan <rmulligan@sitepen.com>
2017-02-20 12:58:38 -0500

    Adding a widget that using dojo/core/request

--- todo-mvc-kitchensink/src/widgets/AppFooter.ts
+++ todo-mvc-kitchensink/src/widgets/AppFooter.ts
@@ -1,10 +1,11 @@
-import { v } from '@dojo/widget-core/d';
+import { v, w } from '@dojo/widget-core/d';
 import { I18nMixin } from '@dojo/widget-core/mixins/I18n';
 import { theme, ThemeableMixin } from '@dojo/widget-core/mixins/Themeable';
 import { WidgetBase } from '@dojo/widget-core/WidgetBase';
 import { SharedWidgetProperties } from '../App';
-import * as styles from './styles/AppFooter.css';
 import appBundle from '../nls/common';
+import { IssuesWidget } from './IssuesWidget';
+import * as styles from './styles/AppFooter.css';
 
 export interface AppFooterProperties extends SharedWidgetProperties {
 }
@@ -14,18 +15,24 @@ export class AppFooter extends I18nMixin(ThemeableMixin(WidgetBase))<AppFooterPr
 	render() {
 		const messages = this.localizeBundle(appBundle);
 
-		return v('footer', {
-			classes: this.classes(styles.footer).get()
-		}, [
-			v('p', {
-				id: 'edit-instructions',
-				innerHTML: messages.footerInstructions
-			}),
-			v('p', {
-				innerHTML: messages.footerCredits
-			}),
-			v('p', {
-				innerHTML: messages.footerPartOf
+		return v('div', {}, [
+			v('footer', {
+				classes: this.classes(styles.footer).get()
+			}, [
+				v('p', {
+					id: 'edit-instructions',
+					innerHTML: messages.footerInstructions
+				}),
+				v('p', {
+					innerHTML: messages.footerCredits
+				}),
+				v('p', {
+					innerHTML: messages.footerPartOf
+				})
+			]),
+			w(IssuesWidget, {
+				user: 'dojo',
+				repo: 'examples'
 			})
 		]);
 	}
