bc679f751d21ed747620382c286a4cbec717314a
Matt Wistrand <matt@troglodytidae.com>
2017-03-16 13:51:39 -0500

    Update packages

--- todo-mvc-kitchensink/src/setLocaleData.ts
+++ todo-mvc-kitchensink/src/setLocaleData.ts
@@ -1,6 +1,5 @@
 import loadCldrData from '@dojo/i18n/cldr/load';
 import { switchLocale } from '@dojo/i18n/i18n';
-import Promise from '@dojo/shim/Promise';
 
 export type AppLocale = 'en' | 'en-PR';
 
