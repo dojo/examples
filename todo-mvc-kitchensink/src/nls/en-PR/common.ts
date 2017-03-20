496add581a71749bf35a5c6e7d3b0119ab39ef75
rorticus <rorticus@gmail.com>
2017-03-20 13:44:37 -0400

    Adding more pirate translation (#199)

--- todo-mvc-kitchensink/src/nls/en-PR/common.ts
+++ todo-mvc-kitchensink/src/nls/en-PR/common.ts
@@ -1,8 +1,23 @@
 const messages = {
-	appTitle: 'things ter do',
+	appTitle: 'Plunderin\' list',
 	createdTitle: 'Sailed on ',
 	detailsTitle: 'Here be more',
-	editPlaceholder: 'wha\' needs t\' be doin?'
+	editPlaceholder: 'wha\' needs t\' be plunderin\'?',
+	clearButtonText: 'Clean up',
+	footerInstructions: 'Double-click fer more',
+	filterCompleted: 'Done',
+	footerCredits: `Credits be to:
+<a href="https://github.com/matt-gadd">Matt Gadd</a>,
+<a href="https://github.com/agubler">Anthony Gubler</a>
+and
+<a href="https://github.com/Tomdye">Tom Dye</a>`,
+	footerPartOf: 'Part of <a href="http://todomvc.com">TodoMVC</a>',
+	itemsLeft: `<strong>{count}</strong><span>{count plural,
+	one {item}
+	other {items} } left</span>`,
+	searchPlaceholder: 'be searchin\'',
+	themeSwitchTitle: 'Pirate Mode'
+
 };
 
 export default messages;
