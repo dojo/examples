131de45a3bba0c4e3db9c7ef16a8359d84e3141e
Anthony Gubler <agubler@sitepen.com>
2017-08-03 11:38:05 +0100

    pull actions out of container

--- todo-mvc-tsx/src/containers/TodoAppContainer.ts
+++ todo-mvc-tsx/src/containers/TodoAppContainer.ts
@@ -1,52 +1,29 @@
-import uuid from '@dojo/core/uuid';
 import { Container } from '@dojo/widget-core/Container';
 import { TodoApp } from './../widgets/TodoApp';
 import { Store } from 'redux';
+import {
+	addTodo,
+	todoInput,
+	removeTodo,
+	toggleTodo,
+	toggleTodos,
+	clearCompleted,
+	editTodo,
+	saveTodo
+} from './../actions/todoActions';
 
 function getProperties(store: Store<any>, properties: any) {
 	const state = store.getState();
 
-	function addTodo() {
-		store.dispatch({ type: 'ADD_TODO', payload: { id: uuid() }});
-	}
-
-	function todoInput(currentTodo: string) {
-		store.dispatch({ type: 'TODO_INPUT', payload: { currentTodo }});
-	}
-
-	function removeTodo(id: string) {
-		store.dispatch({ type: 'DELETE_TODO', payload: { id }});
-	}
-
-	function toggleTodo(id: string) {
-		store.dispatch({ type: 'TOGGLE_TODO', payload: { id }});
-	}
-
-	function clearCompleted() {
-		store.dispatch({ type: 'CLEAR_COMPLETED' });
-	}
-
-	function toggleTodos() {
-		store.dispatch({ type: 'TOGGLE_TODOS' });
-	}
-
-	function editTodo(id: string) {
-		store.dispatch({ type: 'EDIT_TODO', payload: { id }});
-	}
-
-	function saveTodo(id: string, label: string) {
-		store.dispatch({ type: 'SAVE_TODO', payload: { id, label }});
-	}
-
 	return {
-		addTodo,
-		todoInput,
-		removeTodo,
-		toggleTodo,
-		toggleTodos,
-		clearCompleted,
-		editTodo,
-		saveTodo,
+		addTodo: () => store.dispatch(addTodo()),
+		todoInput: (todo: string) => store.dispatch(todoInput(todo)),
+		removeTodo: (id: string) => store.dispatch(removeTodo(id)),
+		toggleTodo: (id: string) => store.dispatch(toggleTodo(id)),
+		toggleTodos: () => store.dispatch(toggleTodos()),
+		clearCompleted: () => store.dispatch(clearCompleted()),
+		editTodo: (id: string) => store.dispatch(editTodo(id)),
+		saveTodo: (id: string, label: string) => store.dispatch(saveTodo(id, label)),
 		currentTodo: state.currentTodo,
 		completedCount: state.completedCount,
 		activeCount: state.activeCount,
