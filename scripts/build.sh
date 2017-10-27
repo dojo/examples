b2494870dddd559b43b1354da120f30a4ecf3a58
Umar Hansa <umar.hansa@gmail.com>
2017-10-27 16:06:20 +0100

    Ensure tests are being run for all projects

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,10 +18,7 @@ else
 	cd $PROJECT_DIR
 	if [ "$PROJECT_DIR" != "dojo-cli-example" ]
 	then
-		if [ "$PROJECT_DIR" != "widget-showcase" ]
-		then
-			runCommandAndCheckStatus ./node_modules/.bin/dojo test -a -c "browserstack"
-		fi
+		runCommandAndCheckStatus ./node_modules/.bin/dojo test -a -c "browserstack"
 		runCommandAndCheckStatus ./node_modules/.bin/dojo build webpack
 	else
 		runCommandAndCheckStatus ./node_modules/.bin/grunt
