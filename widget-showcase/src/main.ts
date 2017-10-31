0291f47482fb24616bd0d229eef313a9ec787a24
Umar Hansa <umar.hansa@gmail.com>
2017-10-31 17:36:14 +0000

    Address code review feedback

--- widget-showcase/src/main.ts
+++ widget-showcase/src/main.ts
@@ -2,8 +2,7 @@ import { ProjectorMixin } from '@dojo/widget-core/mixins/Projector';
 import App from './App';
 import dojoTheme from '@dojo/widgets/themes/dojo/theme';
 
-const root = document.querySelector('showcase') || undefined;
 const Projector = ProjectorMixin(App);
 const projector = new Projector();
 
-projector.append(root);
+projector.append();
