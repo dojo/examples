593f012287fc174c432b2168de29e2f412a8d362
Bradley Maier <bmaier@sitepen.com>
2018-05-10 13:16:52 -0400

    Fix formatting and remove comments

--- resize-observer/src/main.ts
+++ resize-observer/src/main.ts
@@ -8,12 +8,7 @@ if (!global.ResizeObserver) {
 	global.ResizeObserver = ResizeObserver;
 }
 
-// Create a projector to convert the virtual DOM produced by the application into the rendered page.
-// For more information on starting up a Dojo 2 application, take a look at
-// https://dojo.io/tutorials/002_creating_an_application/
 const Projector = ProjectorMixin(App);
 const projector = new Projector();
 
-// By default, append() will attach the rendered content to document.body.  To insert this application
-// into existing HTML content, pass the desired root node to append().
 projector.append();
