d47f9febfe55e7ed2f6bb8744562d456c099d886
Anthony Gubler <agubler@gmail.com>
2018-10-05 04:16:00 -0700

    Ensure the widget examples are controlled correctly (#338)
    * wired
    
    * update function name

--- widget-showcase/src/widgets/tabs/TextInputTab.ts
+++ widget-showcase/src/widgets/tabs/TextInputTab.ts
@@ -1,108 +1,120 @@
 import { v, w } from '@dojo/framework/widget-core/d';
 import { WidgetBase } from '@dojo/framework/widget-core/WidgetBase';
 import TimePicker, { TimeUnits } from '@dojo/widgets/time-picker';
-import TextInput from '@dojo/widgets/text-input';
+import TextInput, { TextInputProperties } from '@dojo/widgets/text-input';
 import EnhancedTextInput from '@dojo/widgets/enhanced-text-input';
 import * as css from '../../styles/tabs.m.css';
+import { Constructor } from '@dojo/framework/widget-core/interfaces';
 
 export default class TextInputTab extends WidgetBase {
-	private _timePickerOptions: TimeUnits[];
-	private _timePickerValue = '10:30';
+	private _state: any = {};
 
-	private _onSetValue(value: string) {
-		this._timePickerValue = value;
-		this.invalidate();
+	private createInput<W extends WidgetBase>(
+		Widget: Constructor<W>,
+		props: W['properties'] & { key: string },
+		options: { cbName: string, initialValue: string } = { cbName: 'onInput', initialValue: '' }
+	) {
+		const { cbName = 'onInput', initialValue = '' } = options;
+		if (!this._state[props.key]) {
+			this._state[props.key] = initialValue;
+		}
+		return w(Widget, {
+			...(props as any),
+			value: this._state[props.key],
+			[cbName]: (value: string) => {
+				this._state[props.key] = value;
+				this.invalidate();
+			}
+		});
 	}
 
-	render() {
+	render() {;
 		return v('div', { classes: css.root }, [
 			v('h2', [ 'Text input' ]),
 			v('div', [
-				w(TextInput, {
+				this.createInput(TextInput, {
 					label: 'Plain text input',
 					type: 'text',
-					placeholder: 'Hello'
+					placeholder: 'Hello',
+					key: 'plain'
 				}),
-				w(TextInput, {
+				this.createInput(TextInput, {
 					label: 'required text input',
 					type: 'text',
 					required: true,
-					placeholder: 'World'
+					placeholder: 'World',
+					key: 'required'
 				}),
-				w(TextInput, {
+				this.createInput(TextInput, {
 					label: 'email input',
 					type: 'email',
-					placeholder: 'Email address'
+					placeholder: 'Email address',
+					key: 'email'
 				}),
-				w(TextInput, {
+				this.createInput(TextInput, {
 					type: 'text',
 					label: 'Disabled input',
 					value: 'Initial value',
 					disabled: true,
-					readOnly: true
+					readOnly: true,
+					key: 'disabled'
 				})
 			]),
 			v('h2', [ 'Enhanced Text input' ]),
 			v('div', [
-				w(EnhancedTextInput, {
+				this.createInput(EnhancedTextInput, {
 					addonBefore: [ '@' ],
 					label: 'Twitter Username',
 					type: 'text',
-					placeholder: 'username'
+					placeholder: 'username',
+					key: 'twitter'
 				}),
-				w(EnhancedTextInput, {
+				this.createInput(EnhancedTextInput, {
 					addonBefore: [ '$' ],
 					addonAfter: [ '.00' ],
 					label: 'Price, rounded to the nearest dollar',
-					type: 'number'
+					type: 'number',
+					key: 'number'
 				})
 			]),
 			v('h2', [ 'Time picker' ]),
 			v('div', [
-				w(TimePicker, {
+				this.createInput(TimePicker, {
 					clearable: true,
 					end: '23:59',
 					label: 'Basic time picker',
-					onChange: this._onSetValue,
-					options: this._timePickerOptions,
 					start: '00:00',
 					step: 1800,
-					value: this._timePickerValue
-				}),
-				w(TimePicker, {
+					key: 'basic-picker'
+				}, { cbName: 'onChange', initialValue: '01:30' }),
+				this.createInput(TimePicker, {
 					clearable: true,
 					end: '23:59',
 					label: 'Basic time picker (native)',
-					onChange: this._onSetValue,
-					options: this._timePickerOptions,
 					start: '00:00',
 					step: 1800,
-					value: this._timePickerValue,
-					useNativeElement: true
-				}),
-				w(TimePicker, {
+					useNativeElement: true,
+					key: 'basic-native-picker'
+				}, { cbName: 'onChange', initialValue: '08:30' }),
+				this.createInput(TimePicker, {
 					clearable: true,
 					disabled: true,
 					end: '23:59',
 					label: 'Disabled time picker',
-					onChange: this._onSetValue,
-					options: this._timePickerOptions,
 					start: '00:00',
 					step: 1800,
-					value: this._timePickerValue
-				}),
-				w(TimePicker, {
+					key: 'disabled-picker'
+				}, { cbName: 'onChange', initialValue: '10:30' }),
+				this.createInput(TimePicker, {
 					clearable: true,
 					disabled: true,
 					end: '23:59',
 					label: 'Disabled time picker (native)',
-					onChange: this._onSetValue,
-					options: this._timePickerOptions,
 					start: '00:00',
 					step: 1800,
-					value: this._timePickerValue,
-					useNativeElement: true
-				})
+					useNativeElement: true,
+					key: 'disabled-native-picker'
+				}, { cbName: 'onChange', initialValue: '11:30' })
 			])
 		]);
 	}
