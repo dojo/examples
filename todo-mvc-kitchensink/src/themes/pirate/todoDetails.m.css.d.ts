f7d2275b157b6ba802c5f6cff19d31d7a320d7f7
Anthony Gubler <agubler@sitepen.com>
2019-06-01 14:38:21 +0100

    add todo details theme css

---
+++ todo-mvc-kitchensink/src/themes/pirate/todoDetails.m.css.d.ts
@@ -0,0 +1,7 @@
+export const backdrop: string;
+export const modal: string;
+export const close: string;
+export const todoDetailsHeader: string;
+export const todoDetailsTextArea: string;
+export const lastUpdated: string;
+export const toggle: string;
