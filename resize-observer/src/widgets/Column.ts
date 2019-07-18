74b5e9b3ae12ab53e0c57ac5d64cdf658ca68b84
Anthony Gubler <agubler@sitepen.com>
2019-07-18 17:23:13 +0100

    Update Resize Example to Dojo 6 (#366)
    * dojo 6 update
    
    * clean up
    
    * default to large
    
    * Remove App unit test

--- resize-observer/src/widgets/Column.ts
+++ resize-observer/src/widgets/Column.ts
@@ -1,24 +1,13 @@
-import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
-import { v } from '@dojo/framework/widget-core/d';
-import Resize, { ContentRect } from '@dojo/framework/widget-core/meta/Resize';
-import * as css from './styles/columns.m.css';
+import { create, v } from "@dojo/framework/core/vdom";
+import breakpoint from "@dojo/framework/core/middleware/breakpoint";
+import * as css from "./styles/columns.m.css";
 
-export class Column extends WidgetBase {
+const factory = create({ breakpoint });
 
-	protected _smallPredicate(contentRect: ContentRect) {
-		return contentRect.width < 500;
-	}
-
-	protected render() {
-		const { isSmall } = this.meta(Resize).get('root', {
-			isSmall: this._smallPredicate
-		});
-
-		return v('div', { key: 'root', classes: [ css.root, isSmall ? css.small : css.big ] }, [
-			v('div', { classes: css.badge }, [ isSmall ? 'small' : 'big' ]),
-			...this.children
-		]);
-	}
-}
-
-export default Column;
+export default factory(function Column({ middleware: { breakpoint }, children }) {
+	const { breakpoint: bp } = breakpoint.get("root", { SM: 0, BG: 500 }) || { breakpoint: "SM" };
+	return v("div", { key: "root", classes: [css.root, bp === "SM" ? css.small : css.big] }, [
+		v("div", { classes: css.badge }, [bp === "SM" ? "small" : "big"]),
+		...children
+	]);
+});
--- resize-observer/src/widgets/ResizableSection.ts
+++ resize-observer/src/widgets/ResizableSection.ts
@@ -1,36 +1,5 @@
-import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
-import { v } from '@dojo/framework/widget-core/d';
-import * as css from './styles/resizableSection.m.css';
-
-export class ResizableSection extends WidgetBase<ResizableSectionProperties> {
-	private _columnClasses: { [ index: number ]: string } = {
-		0: css.zeroColumns,
-		1: css.oneColumn,
-		2: css.twoColumns,
-		3: css.threeColumns,
-		4: css.fourColumns,
-		5: css.fiveColumns,
-		6: css.sixColumns,
-		7: css.sevenColumns,
-		8: css.eightColumns
-	};
-
-	protected render() {
-		const { isSmall, isMedium, columns, expand, shrink } = this.properties;
-
-		return v(
-			'div',
-			{ key: 'root', classes: [ css.root, isSmall ? css.eightColumns : this._columnClasses[columns] ] },
-			[
-				isSmall ? null : v('div', { classes: css.sizeControls }, [
-					v('button', { disabled: columns <= (isMedium ? 2 : 1), onclick: shrink }, [ 'Shrink Component' ]),
-					v('button', { disabled: columns === 8, onclick: expand }, [ 'Expand Component' ])
-				]),
-				...this.children
-			]
-		);
-	}
-}
+import { create, v } from "@dojo/framework/core/vdom";
+import * as css from "./styles/resizableSection.m.css";
 
 export interface ResizableSectionProperties {
 	columns: number;
@@ -40,4 +9,44 @@ export interface ResizableSectionProperties {
 	shrink: () => void;
 }
 
-export default ResizableSection;
+const factory = create().properties<ResizableSectionProperties>();
+
+const columnClasses: { [index: number]: string } = {
+	0: css.zeroColumns,
+	1: css.oneColumn,
+	2: css.twoColumns,
+	3: css.threeColumns,
+	4: css.fourColumns,
+	5: css.fiveColumns,
+	6: css.sixColumns,
+	7: css.sevenColumns,
+	8: css.eightColumns
+};
+
+export default factory(function ResizableSection({ properties, children }) {
+	const { isSmall, isMedium, columns, expand, shrink } = properties;
+
+	return v(
+		"div",
+		{
+			key: "root",
+			classes: [css.root, isSmall ? css.eightColumns : columnClasses[columns]]
+		},
+		[
+			isSmall
+				? null
+				: v("div", { classes: css.sizeControls }, [
+						v(
+							"button",
+							{
+								disabled: columns <= (isMedium ? 2 : 1),
+								onclick: shrink
+							},
+							["Shrink Component"]
+						),
+						v("button", { disabled: columns === 8, onclick: expand }, ["Expand Component"])
+				  ]),
+			...children
+		]
+	);
+});
