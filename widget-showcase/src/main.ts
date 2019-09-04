d7bbade9c1c867b37e048db810b691d6541b1d89
Tom Dye <github@tomdye.co.uk>
2019-09-04 11:53:56 +0100

    bump lint staged

--- widget-showcase/src/main.ts
+++ widget-showcase/src/main.ts
@@ -1,6 +1,7 @@
 import renderer, { w } from '@dojo/framework/core/vdom';
 import App from './App';
 import '@dojo/themes/dojo/index.css';
+import '@dojo/themes/material/index.css';
 
 const r = renderer(() => w(App, {}));
 r.mount();
