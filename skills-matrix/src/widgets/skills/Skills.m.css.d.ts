adad5fb9018faf8f23cb0be53f0ed22711940649
bitpshr <mail@bitpshr.net>
2020-04-29 07:19:48 -0400

    feedback: animationc cleanup

--- skills-matrix/src/widgets/skills/Skills.m.css.d.ts
+++ skills-matrix/src/widgets/skills/Skills.m.css.d.ts
@@ -1,6 +1,7 @@
 export const root: string;
 export const copyButton: string;
 export const success: string;
+export const successVisible: string;
 export const hashInput: string;
 export const layoutRoot: string;
 export const column: string;
